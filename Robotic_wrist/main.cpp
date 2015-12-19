d630b291c4e143ed856ae6541aa4387e2d80aaa7
Lee Sutton <Lee@lees-macbook-pro.local>
2015-12-19 13:52:27 -0800

    Initial Commit

---
+++ Robotic_wrist/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Robotic_wrist
+//
+//  Created by Lee Sutton on 2015-12-19.
+//  Copyright © 2015 Lee Sutton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
